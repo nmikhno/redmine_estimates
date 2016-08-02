071fc03e136015c80841c4fdce8c6ab5263f2c73
Nick Mikhno <nmikhno@evergreens.com.ua>
2016-08-02 19:35:20 +0300

    added issue model patch

--- init.rb
+++ init.rb
@@ -1,8 +1,15 @@
 require 'redmine'
 require_relative 'lib/issues_controller_patch'
+require_relative 'lib/issue_model_patch'
+
 
 ActionDispatch::Callbacks.to_prepare do 
   IssuesController.send :include, IssuesControllerPatch 
+end
+
+
+ActionDispatch::Callbacks.to_prepare do 
+  Issue.send :include, IssueModelPatch 
 end
 
 Redmine::Plugin.register :estimates do
@@ -16,6 +23,6 @@ Redmine::Plugin.register :estimates do
 
   project_module :estimates do
     permission :view_estimates, {:estimates => [:new, :create, :index, :report]}, :public => true
-    permission :edit_estimates, {:estimate_entries => [:edit, :update]}
+    permission :edit_estimates, {:estimate_entries => [:edit, :update, :destroy]}
   end
 end
