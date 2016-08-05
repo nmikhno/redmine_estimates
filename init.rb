1960d0d653dcd12f5b34090b42165469a532676f
Nick Mikhno <nmikhno@evergreens.com.ua>
2016-08-05 19:42:37 +0300

    fix bug of failing rake redmine:plugins:migrate

--- init.rb
+++ init.rb
@@ -12,14 +12,13 @@ ActionDispatch::Callbacks.to_prepare do
   Issue.send :include, IssueModelPatch 
 end
 
-Redmine::Plugin.register :estimates do
+Redmine::Plugin.register :redmine_estimates do
   name 'Estimates plugin'
   author 'Nick Mikhno'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://http://evergreen.team'
+  url 'http://evergreen.team'
   author_url 'http://evergreen.team'
-
 
   project_module :estimates do
     permission :view_estimates, {:estimates => [:new, :create, :index, :report]}, :public => true
