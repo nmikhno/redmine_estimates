5b3d1a5be61c4f9cc92c135880d7403e37a0e434
Nick Mikhno <nmikhno@evergreens.com.ua>
2016-10-02 14:28:00 +0300

    update plugin version and refactor init.rb file for code consistency

--- init.rb
+++ init.rb
@@ -3,32 +3,27 @@ require_relative 'lib/issues_controller_patch'
 require_relative 'lib/issue_model_patch'
 require_relative 'lib/issue_query_patch'
 
-p 'init test'
-
-ActionDispatch::Callbacks.to_prepare do 
-  IssuesController.send :include, IssuesControllerPatch 
+ActionDispatch::Callbacks.to_prepare do
+  IssuesController.send :include, IssuesControllerPatch
 end
 
-
-ActionDispatch::Callbacks.to_prepare do 
-  Issue.send :include, IssueModelPatch 
+ActionDispatch::Callbacks.to_prepare do
+  Issue.send :include, IssueModelPatch
 end
 
-ActionDispatch::Callbacks.to_prepare do 
-  IssueQuery.send :include, IssueQueryPatch 
+ActionDispatch::Callbacks.to_prepare do
+  IssueQuery.send :include, IssueQueryPatch
 end
 
 Redmine::Plugin.register :redmine_estimates do
   name 'Estimates plugin'
   author 'Nick Mikhno'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  description 'This is Redmine plugin for multiple estimate entries for a single task'
+  version '0.9'
   url 'http://evergreen.team'
   author_url 'http://evergreen.team'
 
-  project_module :estimates do
-    permission :view_estimates, {:estimates => [:new, :create, :index, :report]}, :public => true
-    permission :edit_estimates, {:estimate_entries => [:edit, :update, :destroy, :accept]}
-    permission :accept_estimates, {:estimates => :accept}
-  end
+  permission :view_estimates, {:estimates => [:new, :create, :index, :report]}, :public => true
+  permission :edit_estimates, {:estimates => [:edit, :update, :destroy, :accept]}
+  permission :accept_estimates, {:estimates => :accept}
 end
